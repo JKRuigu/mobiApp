511f7358dfcbbc6bbd6bdb44880b1739365cc0c1
JKRuigu <ruigukjohn@gmail.com>
2022-06-03 21:14:51 +0300

    First commit

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <WebView source={{ uri: 'http://mobi-pos.herokuapp.com/' }} />;
 }
 
 const styles = StyleSheet.create({
